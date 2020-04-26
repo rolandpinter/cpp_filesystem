f03d9720acc0f6cea215cd916788aa3387a7d350
Roland Pintér <pinter.roland.ms@gmail.com>
2020-04-26 16:24:05 +0200

    collecting file names and sizes are done

--- GPU_FileSystem_Assignment/functions.hpp
+++ GPU_FileSystem_Assignment/functions.hpp
@@ -14,11 +14,16 @@ namespace fs = std::__fs::filesystem;
 class explorer
 {
 private:
+    // private class member variables
     std::vector<fs::path> m_fileNames;
+    std::vector<unsigned long> m_fileSizes; // dimension = Bytes
+    
     std::vector<fs::path> m_folderNames;
+    std::vector<unsigned long> m_folderSizes;
+    std::vector<unsigned> m_numberOfThingsInAFolder;
     
 public:
-    // Class variables
+    // public class member variables
     std::string m_givenPath;
     
     // Default constructor
@@ -33,22 +38,47 @@ public:
     };
     
     
-    // Class functions
+    // Class member functions
+// (1) manually set the path of the class which should be examined
     void setPath(std::string givenPath)
     {
         m_givenPath = givenPath;
     }
     
+// (2) iterate the path and collect information
     void iterateThroughGivenPath(void)
     {
         for(auto& p: fs::recursive_directory_iterator(m_givenPath))
         {
+            // if not directory
             if(!fs::is_directory(p.symlink_status()))
             {
-                if (p.path().extension() == "")
-                    std::cout << p.path() << std::endl;
+                m_fileNames.push_back(p.path());                // save file names
+                m_fileSizes.push_back(fs::file_size(p.path())); // save file sizes
+            }
+            
+            // if directory: TODO
+            else
+            {
+                continue;
             }
               
+        }
+    }
+    
+// (3) determine the n biggest files
+    void biggestFiles(const unsigned int n)
+    {
+        std::cout << "TODO" <<std::endl;
+    }
+    
+    
+    // just for development phase, sanity checks
+    void printFileInfo(void)
+    {
+        for (int i=0; i<m_fileNames.size(); ++i)
+        {
+            std::cout << m_fileSizes[i] << "\t" << m_fileNames[i] << std::endl;
         }
     }
 
--- GPU_FileSystem_Assignment/main.cpp
+++ GPU_FileSystem_Assignment/main.cpp
@@ -2,20 +2,9 @@
 
 int main(int argc, const char * argv[])
 {
-    /*
-    fs::path pathToShow("/Users/Roli/Desktop/MSc/2.félév");
-    std::cout << "exists() = " << fs::exists(pathToShow) << "\n"
-         << "root_name() = " << pathToShow.root_name() << "\n"
-         << "root_path() = " << pathToShow.root_path() << "\n"
-         << "relative_path() = " << pathToShow.relative_path() << "\n"
-         << "parent_path() = " << pathToShow.parent_path() << "\n"
-         << "filename() = " << pathToShow.filename() << "\n"
-         << "stem() = " << pathToShow.stem() << "\n"
-         << "extension() = " << pathToShow.extension() << "\n";
-    */
-    
-    explorer a("/Users/Roli/Desktop/MSc/2.félév/gpu/");
-    a.iterateThroughGivenPath();
+    explorer foo("/Users/Roli/Desktop/MSc/2.félév/gpu/");
+    foo.iterateThroughGivenPath();
+    //foo.printFileInfo();
     
     return 0;
 }
